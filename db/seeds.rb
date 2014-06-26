c7d14d447fdfd752ff136237f7cc8b8c7aaf24ea
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-06-26 10:07:12 +0200

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+User.create(:username => "admin", :password => "test", :first_name => "Test", :last_name => "User", :user_status => "ACTIVE", :role => "ADMIN")
+
+apps = [
+    {
+      :name => "ANC",
+      :url  => "http://appserver/anc",
+      :icon => "anc.png",
+      :user => (User.first.id rescue 1)
+    },
+    {
+      :name => "ART",
+      :url  => "http://appserver/art",
+      :icon => "art.png",
+      :user => (User.first.id rescue 1)
+    },
+    {
+      :name => "CCC",
+      :url  => "http://appserver/ccc",
+      :icon => "ccc.png",
+      :user => (User.first.id rescue 1)
+    },
+    {
+      :name => "Maternity",
+      :url  => "http://appserver/maternity",
+      :icon => "maternity.png",
+      :user => (User.first.id rescue 1)
+    },
+    {
+      :name => "OPD",
+      :url  => "http://appserver/opd",
+      :icon => "opd.png",
+      :user => (User.first.id rescue 1)
+    },
+    {
+      :name => "Pharmacy",
+      :url  => "http://appserver/pharmacy",
+      :icon => "pharmacy.png",
+      :user => (User.first.id rescue 1)
+    },
+    {
+      :name => "Radiology",
+      :url  => "http://appserver/radiology",
+      :icon => "radiology.png",
+      :user => (User.first.id rescue 1)
+    },
+    {
+      :name => "Under 5",
+      :url  => "http://appserver/u5",
+      :icon => "u5.png",
+      :user => (User.first.id rescue 1)
+    }
+  ]
+  
+  
+apps.each do |app|
+
+  record = Application.create(:application_name => app[:name], :url => app[:url], :icon_link => app[:icon], :user_id => app[:user])
+  
+  src = "#{Rails.root}/db/data/images/#{app[:icon]}"
+  
+  if File.exists?(src)
+  
+    puts "Copying file #{app[:icon]}..."
+  
+    FileUtils.copy(src, "#{Rails.root}/public/data/#{app[:icon]}")
+    
+  end
+
+end
